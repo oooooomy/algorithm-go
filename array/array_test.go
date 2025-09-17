6faf7fdbdce1f21e4a79ca5d5c6f783607a01915
oooooomy <1729677089@qq.com>
2025-09-17 14:15:17 +0800

    169. 多数元素

--- array/array_test.go
+++ array/array_test.go
@@ -1,35 +1,57 @@
 package array
 
 import (
-	"fmt"
-	"log"
 	"testing"
 )
 
 func TestFourSum(t *testing.T) {
 	res := fourSum([]int{1, 0, -1, 0, -2, 2}, 0)
-	fmt.Println(res)
+	t.Log(res)
 }
 
 func TestRemoveElement(t *testing.T) {
 	res := removeElement([]int{3, 2, 2, 3}, 3)
-	log.Println("res: ", res)
+	t.Log("res: ", res)
 	if res != 2 {
 		t.Fail()
 	}
 	res = removeElement([]int{0, 1, 2, 2, 3, 0, 4, 2}, 2)
-	log.Println("res: ", res)
+	t.Log("res: ", res)
 	if res != 5 {
 		t.Fail()
 	}
 	res = removeElementTwo([]int{3, 2, 2, 3}, 3)
-	log.Println("res: ", res)
+	t.Log("res: ", res)
 	if res != 2 {
 		t.Fail()
 	}
 	res = removeElementTwo([]int{0, 1, 2, 2, 3, 0, 4, 2}, 2)
-	log.Println("res: ", res)
+	t.Log("res: ", res)
 	if res != 5 {
 		t.Fail()
+	}
+}
+
+func TestSortedSquares(t *testing.T) {
+	res := sortedSquares([]int{-7, -3, 2, 3, 11})
+	t.Log("res: ", res)
+	a := []int{4, 9, 9, 49, 121}
+	for i, v := range res {
+		if v != a[i] {
+			t.Fail()
+		}
+	}
+}
+
+func TestMinSubArrayLen(t *testing.T) {
+	res := minSubArrayLen(7, []int{2, 3, 1, 2, 4, 3})
+	t.Log("res: ", res)
+	if res != 2 {
+		t.Errorf("\nexpected: %d, output: %d\n", 2, res)
+	}
+	res = minSubArrayLen(11, []int{1, 2, 3, 4, 5})
+	t.Log("res: ", res)
+	if res != 3 {
+		t.Errorf("\nexpected: %d, output: %d\n", 3, res)
 	}
 }
---
+++ tip/tip_test.go
@@ -0,0 +1,14 @@
+package tip
+
+import "testing"
+
+func TestMajorityElement(t *testing.T) {
+	ans := majorityElement([]int{3, 2, 3})
+	if ans != 3 {
+		t.Errorf("\nexpected: %d, output: %d\n", 3, ans)
+	}
+	ans = majorityElement([]int{2, 2, 1, 1, 1, 2, 2})
+	if ans != 2 {
+		t.Errorf("\nexpected: %d, output: %d\n", 2, ans)
+	}
+}
