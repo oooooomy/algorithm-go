c0554fb2fd6479ff28a473e5810a4b92676dc567
oooooomy <1729677089@qq.com>
2025-09-18 15:03:57 +0800

    add sort func

---
+++ sort.go
@@ -0,0 +1,25 @@
+package main
+
+// 冒泡排序
+// 时间复杂度：O(n²)
+func bubbleSort(arr []int) {}
+
+// 选择排序
+// 时间复杂度：O(n²)
+func selectionSort(arr []int) {}
+
+// 插入排序
+// 时间复杂度：O(n²)（最坏情况），O(n)（最好情况）
+func insertionSort(arr []int) {}
+
+// 快速排序
+// 时间复杂度：O(n log n)（平均），O(n²)（最坏）
+func quickSort(arr []int) {}
+
+// 归并排序
+// 时间复杂度：O(n log n)
+func mergeSort(arr []int) {}
+
+// 堆排序
+// 时间复杂度：O(n log n)
+func heapSort(arr []int) {}
