8ca7d8d72972c9646eea99504fba4e5b6fffa694
oooooomy <1729677089@qq.com>
2025-09-21 23:52:21 +0800

    add tree

--- ​​binary_tree/base.go
+++ ​​binary_tree/base.go
@@ -6,3 +6,18 @@ type TreeNode struct {
 	Left  *TreeNode
 	Right *TreeNode
 }
+
+//Preorder 先序遍历
+func Preorder(root *TreeNode) {
+
+}
+
+//Inorder 中序遍历
+func Inorder(root *TreeNode) {
+
+}
+
+//Postorder 后序遍历
+func Postorder(root *TreeNode) {
+
+}
